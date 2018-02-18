7ca5038a9fdf89f4014d0831194c662227f4b7a5
AnthemCS <anthemcreativestudios@gmail.com>
2018-02-19 00:56:18 +0200

    Initial commit

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready( function () {
+    // if($(window).width() < 1200){
+    //     window.location = "";
+    // }
+});
+
+var $ = require('jquery');
+var jsonDB = require('./jsonDB');
+
